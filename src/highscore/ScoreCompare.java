15255d982e7207d64c6e5aa06f8dff16addeff95
Maxwell Wong <maxwell.khwong@gmail.com>
2021-12-13 04:05:43 +0800

    ScoreCompare fixed

--- src/highscore/ScoreCompare.java
+++ src/highscore/ScoreCompare.java
@@ -4,12 +4,7 @@ import java.util.Comparator;
 
 public class ScoreCompare implements Comparator<Score> {
 
-    /**
-     * compare Compares the two numbers and sorts it out accordingly
-     * @param score1 first initialized score to compare
-     * @param score2 second initialized score to compare
-     * @return
-     */
+
     public int compare(Score score1, Score score2) {
 
         int sc1 = score1.getScore();
